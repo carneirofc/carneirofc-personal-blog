e1ade64757f621cc7f0b0b06e259dcd6765ee933
carneirofc <claudiofcarneiro@hotmail.com>
2020-10-25 18:09:49 -0300

    Initial content

---
+++ src/components/layout.jsx
@@ -0,0 +1,26 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <main>{children}</main>
+      <footer style={{ marginTop: `2rem` }}>
+        © {new Date().getFullYear()}, Built with {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </>
+  );
+};
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
