3613d7bc3656f3e1f713e1d2af1a16d71d4dde93
carneirofc <claudiofcarneiro@hotmail.com>
2020-11-15 15:25:05 -0300

    Fix SocialLinks

--- src/components/SocialLinks/content.jsx
+++ src/components/SocialLinks/content.jsx
@@ -1,30 +1,29 @@
-import Icons from "./Icons";
-const links = [
-  {
-    url: "https://github.com/carneirofc",
-    label: "Github",
-    Icon: Icons.Github,
-  },
-  {
-    url: "https://www.linkedin.com/in/cl%C3%A1udio-carneiro-458b27195",
-    label: "Linkedin",
-    Icon: Icons.Linkedin,
-  },
-  {
-    url: "carneirofc#5793",
-    label: "Discord",
-    Icon: Icons.Discord,
-  },
-  {
-    url: "https://www.instagram.com/claudio.f.carneiro/",
-    label: "Instagram",
-    Icon: Icons.Instagram,
-  },
-  {
-    url: "",
-    label: "Battlenet",
-    Icons: Icons.BattleNet,
-  },
-];
+const github = {
+  url: "https://github.com/carneirofc",
+  label: "Github",
+};
 
-export default links;
+const linkedin = {
+  url: "https://www.linkedin.com/in/cl%C3%A1udio-carneiro-458b27195",
+  label: "Linkedin",
+};
+const discord = {
+  url: "https://discordapp.com/users/425464719206252546",
+  label: "Discord",
+};
+const instagram = {
+  url: "https://www.instagram.com/claudio.f.carneiro/",
+  label: "Instagram",
+};
+const battlenet = {
+  url: null,
+  label: "Battlenet",
+};
+
+export default {
+  github,
+  linkedin,
+  discord,
+  instagram,
+  battlenet,
+};
--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import links from "./content";
+import Icons from "./Icons";
 
 import * as S from "./styled";
 
-const renderLink = () => {
-  return links.map((data, i) => {
-    const { label, url, Icon } = data;
-    return (
-      <S.SocialLinksItem key={i}>
-        <S.SocialLinksLink href={url} title={label} target="_blank" rel="noopener noreferrer">
-          <S.IconWarpper>{Icon}</S.IconWarpper>
-        </S.SocialLinksLink>
-      </S.SocialLinksItem>
-    );
-  });
+const renderLink = (Icon, data) => {
+  const { label, url } = data;
+
+  return (
+    <S.SocialLinksItem>
+      <S.SocialLinksLink href={url} title={label} target="_blank" rel="noopener noreferrer">
+        <S.IconWarpper>
+          <Icon />
+        </S.IconWarpper>
+      </S.SocialLinksLink>
+    </S.SocialLinksItem>
+  );
 };
 
 const SocialLinks = () => {
   return (
     <S.SocialLinksWrapper>
-      <S.SocialLinksList>{renderLink()}</S.SocialLinksList>
+      <S.SocialLinksList>
+        {renderLink(Icons.Github, links.github)}
+        {renderLink(Icons.Discord, links.discord)}
+        {renderLink(Icons.Instagram, links.instagram)}
+        {renderLink(Icons.Linkedin, links.linkedin)}
+        {renderLink(Icons.BattleNet, links.battlenet)}
+      </S.SocialLinksList>
     </S.SocialLinksWrapper>
   );
 };
